e7bf45e92ac878bacb0a11a9d57bf383c90e9adf
Chi Tinh <tinhnguyen23122004@gmail.com>
2024-07-18 18:13:47 +0700

    upload file

---
+++ opt.py
@@ -0,0 +1,18 @@
+import argparse
+
+
+def get_opt():
+    """
+    A function to get training parameters using argparse and return the parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description="Get training parameters.")
+
+    parser.add_argument('--root_dir', type=str, required=True, help='Path to the root directory.')
+    parser.add_argument('--size', type=int, required=True, help='Size of the input images.')
+    parser.add_argument('--batch_size', type=int, required=True, help='Batch size for training.')
+    parser.add_argument('--epoch', type=int, required=True, help='Number of epochs for training.')
+    parser.add_argument('--export_dir', type=str, default=None, help='Path to export the model.')
+    parser.add_argument('--h5_dir', type=str, default=None, help='Path to the H5 model file.')
+
+    return parser.parse_args()
+
---
+++ train_multiple_model.py
@@ -0,0 +1,115 @@
+import keras
+import os
+from model import get_model, get_augmentation
+from opt import get_opt
+from keras.api.preprocessing import image_dataset_from_directory
+
+
+def get_dataset(root_dir, size, batch_size):
+    """
+    Retrieves datasets for training, validation, and testing from the specified root directory.
+
+    Parameters:
+    - root_dir: The root directory containing the datasets.
+    - size: The size of the input images.
+    - batch_size: The batch size for the datasets.
+
+    Returns:
+    A tuple containing the training dataset, validation dataset, and testing dataset.
+    """
+    train_dataset = image_dataset_from_directory(directory=f"{root_dir}/train",
+                                                 shuffle=True,
+                                                 image_size=size,
+                                                 batch_size=batch_size,
+                                                 label_mode="categorical")
+    val_dataset = image_dataset_from_directory(directory=f"{root_dir}/val",
+                                               shuffle=True,
+                                               image_size=size,
+                                               batch_size=batch_size, label_mode="categorical")
+    test_dataset = image_dataset_from_directory(directory=f"{root_dir}/test",
+                                                shuffle=True,
+                                                image_size=size,
+                                                batch_size=batch_size, label_mode="categorical")
+
+    augmentation = get_augmentation()
+
+    train_ds = train_ds.map(lambda x, y: (augmentation(x, training=True), y))
+    val_ds = val_ds.map(lambda x, y: (augmentation(x, training=True), y))
+    train_ds = train_ds.cache().shuffle(len(train_ds))
+    val_ds = val_ds.cache().shuffle(len(train_ds))
+
+
+
+    return train_dataset, val_dataset, test_dataset
+
+
+def train_single(model, train_ds, val_ds, epoch, export_dir=None, h5_dir=None):
+    """
+    Trains a single model using the provided dataset and parameters.
+
+    Parameters:
+    - model: The model to train.
+    - train_ds: The training dataset.
+    - val_ds: The validation dataset.
+    - epoch: The number of epochs to train.
+    - export_dir: The directory to export the trained model.
+    - h5_dir: The directory to save the model in h5 format.
+
+    Returns:
+    None
+    """
+    model.fit(train_ds, epochs=epoch, validation_data=val_ds)
+    if export_dir:
+        print(f"Export model to {export_dir}")
+        model.export(f"{export_dir}/{model.name}")
+    if h5_dir:
+        print(f"Save model to {h5_dir}")
+        model.save(f"{h5_dir}/{model.name}.h5")
+
+
+def train_disease_classification(root_dir, size, batch_size, epoch, export_dir=None, h5_dir=None):
+    """
+    Trains a disease classification model based on datasets in the specified root directory.
+
+    Parameters:
+    - root_dir: The root directory containing the datasets.
+    - size: The size of the input images.
+    - batch_size: The batch size for training.
+    - epoch: The number of epochs to train the model.
+    - export_dir: Path to export the trained model.
+    - h5_dir: Path to save the model in h5 format.
+
+    Returns:
+    None
+    """
+    list_dataset_name = os.listdir(root_dir)
+    print(f"Found {len(list_dataset_name)} dataset in {root_dir}")
+    print("Start training...")
+
+    for name in list_dataset_name:
+        model_name = f"{name}_classification"
+        data_root = f"{root_dir}/{name}"
+        print(f"Get train, val, test ds in {data_root}")
+        train_ds, val_ds, test_ds = get_dataset(f"{root_dir}/{name}", (size, size), batch_size)
+
+        class_names = train_ds.class_names
+        
+        print(f"Get model for {name} ds")
+        model = get_model((size, size, 3), len(class_names), model_name)
+
+        model.compile(
+            loss=keras.losses.CategoricalCrossentropy(),
+            optimizer=keras.optimizers.Adam(learning_rate=0.001),
+            metrics=["accuracy"], )
+
+        print(f"Training model with {epoch} epoch")
+        train_single(model, train_ds, val_ds, epoch, export_dir, h5_dir)
+
+
+def trainer():
+    opt = get_opt()
+    train_disease_classification(**vars(opt))
+
+
+if __name__ == "__main__":
+    trainer()
